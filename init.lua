f21e31c6e0e8da5ff455d2b2a122f04c481d319e
Megaf <mmegaf@gmail.com>
2014-11-09 23:25:50 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,21 @@
 -- Anvil mod. By Megaf.
+minetest.register_node("megaf_anvil:anvil", {
+  description = "Falling Anvil",
+  tiles = {"anvil.png"},
+  inventory_image = {"anvil_inventory.png"},
+  groups = {crumbly=2, falling_node=1}
+  drop = 'transparent_nodes:transparent_blue',
+  legacy_mineral = true,
+  sounds = default.node_sound_stone_defaults(),
+  damage_per_second = 16,  
+})
+
+minetest.register_craft({
+  output = 'megaf_anvil:anvil',
+    recipe = {
+    {'default:steel_block', 'default:steel_block'},
+    {'default:steel_ingot', 'default:steel_ingot'},
+    {'default:wood', 'default:wood'},
+    }
+})
+
