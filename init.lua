726b2e8b6c93d5a5564c3333ecba5546d9f0401b
Megaf <mmegaf@gmail.com>
2014-11-09 23:48:42 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,28 @@
 -- Anvil mod. By Megaf.
 minetest.register_node("megaf_anvil:anvil", {
   description = "Falling Anvil",
-  tiles = {"anvil.png"},
+	tiles = {
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png",
+		"default_wood.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0, -0.3125, 0.125, 0.1875, 0.3125}, -- NodeBox1
+			{-0.0625, 0.0625, -0.4375, 0.0625, 0.1875, -0.3125}, -- NodeBox5
+			{-0.125, 0.0625, 0.1875, 0.125, 0.1875, 0.5}, -- NodeBox6
+			{-0.125, -0.375, -0.25, 0.125, -0.1875, 0.125}, -- NodeBox7
+			{-0.1875, -0.4375, -0.375, 0.1875, -0.375, 0.4375}, -- NodeBox8
+			{-0.25, -0.5, -0.5, 0.25, -0.4375, 0.5}, -- NodeBox9
+			{-0.0625, -0.1875, -0.25, 0.0625, 0, 0.125}, -- NodeBox10
+		}
+	}
   inventory_image = {"anvil_inventory.png"},
   groups = {crumbly=2, falling_node=1}
   drop = 'transparent_nodes:transparent_blue',
