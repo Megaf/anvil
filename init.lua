a2e1b07f1569b4c77fd6e346a6c9633600d2259a
Megaf <mmegaf@gmail.com>
2017-11-23 14:17:55 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,25 +1,33 @@
+
 -- Anvil mod. By Megaf.
 minetest.register_node("anvil:anvil", {
   description = "Falling Anvil",
-	tiles = {"anvil.png"},
-	drawtype = "nodebox",
-	paramtype = "light",
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.125, 0, -0.3125, 0.125, 0.1875, 0.3125}, -- NodeBox1
-			{-0.0625, 0.0625, -0.4375, 0.0625, 0.1875, -0.3125}, -- NodeBox5
-			{-0.125, 0.0625, 0.1875, 0.125, 0.1875, 0.5}, -- NodeBox6
-			{-0.125, -0.375, -0.25, 0.125, -0.1875, 0.125}, -- NodeBox7
-			{-0.1875, -0.4375, -0.375, 0.1875, -0.375, 0.4375}, -- NodeBox8
-			{-0.25, -0.5, -0.5, 0.25, -0.4375, 0.5}, -- NodeBox9
-			{-0.0625, -0.1875, -0.25, 0.0625, 0, 0.125}, -- NodeBox10
-			},
-		},
-	groups = {cracky=2, falling_node=1},
-	drop = 'anvil:anvil',
-	legacy_mineral = true,
-	damage_per_second = 20,  
+        tiles = {
+                "anvil.png",
+                "anvil.png",
+                "anvil.png",
+                "anvil.png",
+                "anvil.png",
+                "anvil.png"
+        },
+        drawtype = "nodebox",
+        paramtype = "light",
+        node_box = {
+                type = "fixed",
+                fixed = {
+                        {-0.125, 0, -0.3125, 0.125, 0.1875, 0.3125}, -- NodeBox1
+                        {-0.0625, 0.0625, -0.4375, 0.0625, 0.1875, -0.3125}, -- NodeBox5
+                        {-0.125, 0.0625, 0.1875, 0.125, 0.1875, 0.5}, -- NodeBox6
+                        {-0.125, -0.375, -0.25, 0.125, -0.1875, 0.125}, -- NodeBox7
+                        {-0.1875, -0.4375, -0.375, 0.1875, -0.375, 0.4375}, -- NodeBox8
+                        {-0.25, -0.5, -0.5, 0.25, -0.4375, 0.5}, -- NodeBox9
+                        {-0.0625, -0.1875, -0.25, 0.0625, 0, 0.125}, -- NodeBox10
+                        },
+                },
+        groups = {cracky=2, falling_node=1},
+        drop = 'anvil:anvil',
+        legacy_mineral = true,
+        damage_per_second = 20,
 })
 
 minetest.register_craft({
@@ -30,4 +38,3 @@ minetest.register_craft({
     {'default:wood', 'default:wood'},
     }
 })
-print('[anvil] loaded.')
